b5d226ef4cb49b380d9e74ad75ecedb8c22bcfd5
Esteban Vallejo <esteban@infinixsoft.com>
2018-12-28 14:20:52 -0300

    Added DequeuableRegistrable protocol

---
+++ DequeuableRegistrable/Classes/DequeuableRegistrable.swift
@@ -0,0 +1,10 @@
+//
+//  DequeuableRegistrable.swift
+//  DequeuableRegistrable
+//
+//  Created by Steve on 28/12/2018.
+//
+
+import Foundation
+
+public protocol DequeuableRegistrable: Dequeuable, Registrable {}
